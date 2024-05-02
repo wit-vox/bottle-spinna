ff00e38dceea9ec919c3e41888baedb7455a3617
Claudio Paladini <clode.paladini@gmail.com>
2024-05-02 11:24:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 
+function spinBottle() {
+    const bottle = document.getElementById('bottle');
+    let randomSpin = Math.floor(Math.random() * 360) + 1440; // 1440 degrees ensures it spins at least 4 times
+    bottle.style.transform = `rotate(${randomSpin}deg)`;
+}
