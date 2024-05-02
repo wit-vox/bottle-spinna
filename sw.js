197721973d4fc46d82350bfc766bfdb0ffb15d71
Claudio Paladini <clode.paladini@gmail.com>
2024-05-02 11:40:12 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,13 @@
+var GHPATH = '/bottle-spinna';
+
+var APP_PREFIX = 'rnp_';
+ 
+var VERSION = 'version_00';
+ 
+var URLS = [    
+  `${GHPATH}/`,
+  `${GHPATH}/bottle.png`,
+  `${GHPATH}/index.html`,
+  `${GHPATH}/styles.css`,
+  `${GHPATH}/script.js`
+ ]
